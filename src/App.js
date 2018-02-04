cd9fc4da55dd380d54c1455aa515999c882651b8
Michael Ozoemena <michaelozoemena2@gmail.com>
2018-02-04 19:48:06 +0100

    add a few basic template files

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Button } from 'semantic-ui-react'
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        Hello
+      </div>
+    );
+  }
+}
+
+export default App;
